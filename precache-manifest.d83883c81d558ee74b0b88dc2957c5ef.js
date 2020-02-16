c2d8bbddf088ae415f853b8b6b55932859d85ad5
Angrey Shoful <shoful.a@gmail.com>
2020-02-16 10:27:59 +0200

    Updates

---
+++ precache-manifest.d83883c81d558ee74b0b88dc2957c5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0bd859565fa5f0a06e11c5f8395934",
+    "url": "/AShoful/testCodempire/index.html"
+  },
+  {
+    "revision": "cbc4ef29672d4791f127",
+    "url": "/AShoful/testCodempire/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "768ae9d781f97388413a",
+    "url": "/AShoful/testCodempire/static/js/2.2ac75d1e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/AShoful/testCodempire/static/js/2.2ac75d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc4ef29672d4791f127",
+    "url": "/AShoful/testCodempire/static/js/main.34a699b9.chunk.js"
+  },
+  {
+    "revision": "47ab2a39c0ab0202100f",
+    "url": "/AShoful/testCodempire/static/js/runtime-main.135f4ad9.js"
+  }
+]);
